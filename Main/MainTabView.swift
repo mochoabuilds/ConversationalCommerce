4599d370c9ed0d590db1baaec38581ade3dbec92
Michael Valentino Ochoa <40745550+mochoabuilds@users.noreply.github.com>
2021-08-12 15:56:51 -0500

    Add files via upload

---
+++ Main/MainTabView.swift
@@ -0,0 +1,63 @@
+//
+//  MainTabView.swift
+//  SwiftUIChatTutorial
+//
+//  Created by Michael Valentino Ochoa on 8/12/21
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    @State private var selectedIndex = 0
+    @EnvironmentObject var viewModel: AuthViewModel
+    
+    var body: some View {
+        if let user = viewModel.currentUser {
+            NavigationView {
+                TabView(selection: $selectedIndex) {
+                    ConversationsView()
+                        .onTapGesture {
+                            selectedIndex = 0
+                        }
+                        .tabItem { Image(systemName: "bubble.left") }
+                        .tag(0)
+                    
+                    ChannelsView()
+                        .onTapGesture {
+                            selectedIndex = 1
+                        }
+                        .tabItem { Image(systemName: "bubble.left.and.bubble.right") }
+                        .tag(1)
+
+                    
+                    SettingsView(user: user)
+                        .onTapGesture {
+                            selectedIndex = 2
+                        }
+                        .tabItem { Image(systemName: "gear") }
+                        .tag(2)
+
+                }
+                .navigationTitle(tabTitle)
+                
+            }
+        } else {
+            // show failed to load or empty state view
+        }
+    }
+    
+    var tabTitle: String {
+        switch selectedIndex {
+        case 0: return "Chats"
+        case 1: return "Channels"
+        case 2: return "Settings"
+        default: return ""
+        }
+    }
+}
+
+struct MainTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTabView()
+    }
+}
