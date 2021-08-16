94b74984544d6f5c37f159f5d2aec770b8f27f66
Michael Valentino Ochoa <40745550+mochoabuilds@users.noreply.github.com>
2021-08-16 11:48:06 -0500

    Add files via upload

---
+++ Channels/ChannelsView.swift
@@ -0,0 +1,19 @@
+//
+//  ChannelsView.swift
+//
+//  Created by Michael Valentino Ochoa on 8/16/21
+//
+
+import SwiftUI
+
+struct ChannelsView: View {
+    var body: some View {
+        Text("Channels view goes here..")
+    }
+}
+
+struct ChannelsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChannelsView()
+    }
+}
